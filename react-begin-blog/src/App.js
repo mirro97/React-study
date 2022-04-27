a1c1c1649b713baa441a07c363d16ee2ec336db6
sujeong.k <45249021+mirro97@users.noreply.github.com>
2022-04-28 00:10:38 +0900

    리액트 데이터 바인딩, css 넣기

--- react-begin-blog/src/App.js
+++ react-begin-blog/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// 리액트는 데이터 바인딩이 굉장히 쉬움
+// 중괄호{}를 사용하면 손쉽게 변수를 넣을 수 있음
 
 function App() {
+  let posts = "첫번째 게시글";
+  let image =
+    "https://img.huffingtonpost.com/asset/6168c6ce2000008f3f8d05a7.jpeg?ops=scalefit_720_noupscale";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <div className="title">리액트 Blog</div>
+      </div>
+      <span style={{ fontSize: "17px", color: "#495057" }}>{posts}</span>
+      <img src={image}></img>
     </div>
   );
 }
