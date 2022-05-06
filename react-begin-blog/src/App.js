0d63495327e67cad7b98f50856f6728286043f5f
sujeong.k <45249021+mirro97@users.noreply.github.com>
2022-05-07 02:12:00 +0900

    폰트 적용 및 데이터 추가, 버튼 클릭 이벤트 추가

--- react-begin-blog/src/App.js
+++ react-begin-blog/src/App.js
@@ -8,23 +8,75 @@ function App() {
   // let [작명(=데이터를 담고있음), 작명(=state의 변경을 도와주는 함수)] = useState(보관할 자료);
   // state 는 값이 변경되면 자동으로 html이 재랜더링이 됨!
   // 자주 변경되는 html 부분은 state로 만들어 놓으면 된다
-  let [test, setTest] = useState("아아메가 깔끔한 카페 추천");
   let [title, setTitle] = useState([
-    "페퍼로니 피자가 작살나는 가게",
-    "육즙 잘잘 벌집 삼겹살 맛집",
-    "엄마가 극찬한 밀크티 맛집",
+    {
+      title: "페퍼로니 피자가 작살나는 가게",
+      date: "2022-03-21",
+      writer: "뉴욕치킨",
+    },
+    {
+      title: "육즙 잘잘 벌집 삼겹살 맛집",
+      date: "2022-03-29",
+      writer: "뉴욕치킨",
+    },
+    {
+      title: "엄마가 극찬한 밀크티 맛집",
+      date: "2022-04-6",
+      writer: "뉴욕치킨",
+    },
   ]);
-  let writer = "뉴욕치킨";
+
+  let [likeCount, setCount] = useState(0);
+
   return (
     <div className="App">
       <div className="nav">
         <h2>리액트 Blog</h2>
       </div>
       <div className="list">
-        <h3>{title}</h3>
+        <h3>{title[0].title}</h3>
         <div className="list-bottom">
-          <span>{writer}</span>
-          <p>2022년 5월 7일</p>
+          <div className="regist">
+            <span>{title[0].writer}</span>
+            <p>{title[0].date}</p>
+          </div>
+          <div className="like">
+            {/* onClick 안에는 함수를 넣어야함 */}
+            <span
+              onClick={() => {
+                setCount(likeCount + 1);
+              }}
+            >
+              💙
+            </span>
+            <p>{likeCount}</p>
+          </div>
+        </div>
+      </div>
+      <div className="list">
+        <h3>{title[1].title}</h3>
+        <div className="list-bottom">
+          <div className="regist">
+            <span>{title[1].writer}</span>
+            <p>{title[1].date}</p>
+          </div>
+          <div className="like">
+            <span>💙</span>
+            <p>{likeCount}</p>
+          </div>
+        </div>
+      </div>
+      <div className="list">
+        <h3>{title[2].title}</h3>
+        <div className="list-bottom">
+          <div className="regist">
+            <span>{title[2].writer}</span>
+            <p>{title[2].date}</p>
+          </div>
+          <div className="like">
+            <span>💙</span>
+            <p>{likeCount}</p>
+          </div>
         </div>
       </div>
     </div>
